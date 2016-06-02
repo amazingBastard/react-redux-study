35d07a0c15853b7766c2f09582ce4010194f9be1
amazingBastard <dvazgird@beenverified.com>
2016-06-02 15:20:29 -0400

    youtube api key

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+// const vars usually start with caps and employ underscores
+const API_KEY = 'AIzaSyDGxBKjefeiJrJ4hJhPH821UBJIO-ufMSw';
+
 // create a new component. this component should produce some HTML
 const App = () => {
   return <h1 className="title">Hello World!</h1>;
-};
+}
 
 // take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
