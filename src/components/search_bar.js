f63afd51d5eeda967731a378f8b104f4cd235b04
amazingBastard <dvazgird@beenverified.com>
2016-06-02 15:37:30 -0400

    searchbar wip

---
+++ src/components/search_bar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// define a new class called SearchBar and
+// give it access to all the functionality that React.Component has
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <input />
+    );
+  }
+}
+
+export default SearchBar;
