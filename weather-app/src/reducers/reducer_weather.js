06fcf2740188e86dd96f7f7d605ca00bafe9e207
amazingBastard <dvazgird@beenverified.com>
2016-06-13 17:38:39 -0400

    review

--- weather-app/src/reducers/reducer_weather.js
+++ weather-app/src/reducers/reducer_weather.js
@@ -3,6 +3,7 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
+      // do this to avoid state mutation (state.push would mutate it)
       return [ action.payload.data, ...state ]; // return state.concat([action.payload.data]);
   }
 
