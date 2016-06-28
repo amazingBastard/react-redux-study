6129e149a92e0ef575335d24e1e8d82943aa93a0
amazingBastard <dvazgird@beenverified.com>
2016-06-28 12:09:04 -0400

    posts reducer and posts index component

--- blog-app/src/components/posts_index.js
+++ blog-app/src/components/posts_index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return (
-    <div>
-      List of Blog Posts
-    </div>
-  );
-};
+class PostsIndex extends Component {
+  // only called once before the component will be rendered
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>
+        List of blog posts
+      </div>
+    );
+  }
+}
+
+export default connect(null, { fetchPosts })(PostsIndex);
